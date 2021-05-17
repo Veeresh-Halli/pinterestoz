62f549f61198223e32da93743bb719696963aaf4
Veeresh Halli <77663460+Veeresh-Halli@users.noreply.github.com>
2021-05-17 12:41:36 +0530

    Added domain name

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g$5oqykew+*_%cs98*sn9vj=1nqww^-n!^n9aozmza!$5--ro)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['pinterestoz.herokuapp.com','127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','www.pinterestoz.com','pinterestoz.com']
 
 
 # Application definition
